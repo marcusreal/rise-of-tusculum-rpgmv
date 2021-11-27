8d6a0b2137653d107c28f01dac5e437372ada652
melt99 <crstn.salvati@gmail.com>
2021-11-27 15:30:53 +0100

    Update TXT tilesets And configuration mklink

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Rise Of Tusculum Rpgmv
 #INSTALLATION
 - clone project repository rise-of-tusculum-rpgmv using "git clone https://github.com/cristianosalvati/rise-of-tusculum-rpgmv.git", point the default branch
 - clone repository rise-of-tusculum-media lib using "git clone https://github.com/cristianosalvati/rise-of-tusculum-media-lib.git", point the default branch
-- run the config.bat scritp to set project properties
+- run the config.bat scritp as admin to set project properties
--- config.bat
+++ config.bat
@@ -1,6 +1,6 @@
 echo "Script di configurazione link simbolici"
-mklink /D %cd%\..\rise-of-tusculum-media-lib\audio %cd%\ROT-21-0\audio
-mklink /D %cd%\..\rise-of-tusculum-media-lib\icon %cd%\ROT-21-0\icon
-mklink /D %cd%\..\rise-of-tusculum-media-lib\movies %cd%\ROT-21-0\movies
-mklink /D %cd%\..\rise-of-tusculum-media-lib\img %cd%\ROT-21-0\img
-pause 
+mklink /D  "%cd%\ROT-21-0\audio"  "%cd%\..\rise-of-tusculum-media-lib\audio" 
+mklink /D  "%cd%\ROT-21-0\icon"   "%cd%\..\rise-of-tusculum-media-lib\icon"  
+mklink /D  "%cd%\ROT-21-0\movies" "%cd%\..\rise-of-tusculum-media-lib\movies"
+mklink /D  "%cd%\ROT-21-0\img"    "%cd%\..\rise-of-tusculum-media-lib\img"   
+pause
