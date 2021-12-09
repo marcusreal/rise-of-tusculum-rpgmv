18d93e10f11b049e49fabced11b6576501a54c96
hermannbarbato <hermannbarbato@gmail.com>
2021-12-09 19:50:36 +0100

    Update con mappe base
    Aggiunte 5 mappe, non tutte utili ma forniscono un punto di partenza

--- config.bat
+++ config.bat
@@ -1,6 +1,6 @@
 echo "Script di configurazione link simbolici"
-mklink /D  "%cd%\ROT-21-0\audio"  "%cd%\..\rise-of-tusculum-media-lib\audio" 
-mklink /D  "%cd%\ROT-21-0\icon"   "%cd%\..\rise-of-tusculum-media-lib\icon"  
-mklink /D  "%cd%\ROT-21-0\movies" "%cd%\..\rise-of-tusculum-media-lib\movies"
-mklink /D  "%cd%\ROT-21-0\img"    "%cd%\..\rise-of-tusculum-media-lib\img"   
+mklink /D  "C:\Users\herma\OneDrive - Universita' degli Studi di Roma Tor Vergata\Documenti\GitHub\rise-of-tusculum-rpgmv\ROT-21-0\audio"  "C:\Users\herma\OneDrive - Universita' degli Studi di Roma Tor Vergata\Documenti\GitHub\rise-of-tusculum-rpgmv\..\rise-of-tusculum-media-lib\audio" 
+mklink /D  "C:\Users\herma\OneDrive - Universita' degli Studi di Roma Tor Vergata\Documenti\GitHub\rise-of-tusculum-rpgmv\ROT-21-0\icon"   "C:\Users\herma\OneDrive - Universita' degli Studi di Roma Tor Vergata\Documenti\GitHub\rise-of-tusculum-rpgmv\..\rise-of-tusculum-media-lib\icon"  
+mklink /D  "C:\Users\herma\OneDrive - Universita' degli Studi di Roma Tor Vergata\Documenti\GitHub\rise-of-tusculum-rpgmv\ROT-21-0\movies" "C:\Users\herma\OneDrive - Universita' degli Studi di Roma Tor Vergata\Documenti\GitHub\rise-of-tusculum-rpgmv\..\rise-of-tusculum-media-lib\movies"
+mklink /D  "C:\Users\herma\OneDrive - Universita' degli Studi di Roma Tor Vergata\Documenti\GitHub\rise-of-tusculum-rpgmv\ROT-21-0\img"    "C:\Users\herma\OneDrive - Universita' degli Studi di Roma Tor Vergata\Documenti\GitHub\rise-of-tusculum-rpgmv\..\rise-of-tusculum-media-lib\img"   
 pause
